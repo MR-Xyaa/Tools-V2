e70261892fd3f6473a27e52f9c0dfcb951df5d73
Inu Ganteng <102247436+MR-Xyaa@users.noreply.github.com>
2022-04-04 13:40:12 +0700

    Create logo.py

---
+++ module/logo.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+###################
+## WARNA ASU #####
+P = '\x1b[1;97m' # PUTIH
+M = '\x1b[1;91m' # MERAH
+H = '\x1b[1;92m' # HIJAU
+K = '\x1b[1;93m' # KUNING
+B = '\x1b[1;94m' # BIRU
+U = '\x1b[1;95m' # UNGU
+O = '\x1b[1;96m' # BIRU MUDA
+N = '\x1b[0m'    # WARNA MATI
+##################
+
+
+tolol =("""
+\x1b[1;92m╔═══════[  \x1b[1;91mMR-Xyaa \x1b[1;92m]═════════════════════════════╗
+\x1b[1;92m■  \x1b[1;93m[▪]   \x1b[1;94mAuthor : MR-Xyaa                        \x1b[1;92m■
+\x1b[1;92m■  \x1b[1;93m[▪]   \x1b[1;94mFacbook : Xyaa Xyaa                     \x1b[1;92m■
+\x1b[1;92m■  \x1b[1;93m[▪]   \x1b[1;94mGithub : https://github.com/MR-Xyaa     \x1b[1;92m■
+\x1b[1;92m╚═══════[   XXXXXX ]═════════════════════════════╝
+""")
+def kntl():
+       os.system('clear')
+print (tolol)
