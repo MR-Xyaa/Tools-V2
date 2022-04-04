f76a4c966e4603ba18d9bebea88fd71ea7547031
Inu Ganteng <102247436+MR-Xyaa@users.noreply.github.com>
2022-04-04 13:12:09 +0700

    Create list.py

---
+++ module/list.py
@@ -0,0 +1,52 @@
+# WARNA ASU #####
+P = '\x1b[1;97m' # PUTIH
+M = '\x1b[1;91m' # MERAH
+H = '\x1b[1;92m' # HIJAU
+K = '\x1b[1;93m' # KUNING
+B = '\x1b[1;94m' # BIRU
+U = '\x1b[1;95m' # UNGU
+O = '\x1b[1;96m' # BIRU MUDA
+N = '\x1b[0m'    # WARNA MATI
+##################
+
+import random
+import sys
+import time
+import os
+def inu_ganteng_banget(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.1)
+
+
+user_reply = input("Namamu Bang? \n")
+inu_ganteng_banget('__________  ____  __   _____    _    _____')
+inu_ganteng_banget(' /_  __/ __ \/ __ \/ /  / ___/   | |  / /__')
+inu_ganteng_banget('  / / / / / / / / / /   \__ \    | | / /__/ /')
+inu_ganteng_banget(' / / / /_/ / /_/ / /______/ /    | |/ // __/')
+inu_ganteng_banget('/_/  \____/\____/_____/____/     |___//____/')
+inu_ganteng_banget('                         By MR-Xyaa'
+
+inu_ganteng_banget('1 Red Hawk')
+inu_ganteng_banget('2 D-Tect')
+inu_ganteng_banget('3 Hunner')
+inu_ganteng_banget('4 WPScan')
+inu_ganteng_banget('5 Webdav')
+inu_ganteng_banget('6 Metasploit')
+inu_ganteng_banget('7 Kali Nethunter')
+inu_ganteng_banget('8 Ubuntu')
+inu_ganteng_banget('9 Youtube Dl')
+inu_ganteng_banget('10 viSQL')
+inu_ganteng_banget('11 Weeman')
+inu_ganteng_banget('12 WFDroid')
+inu_ganteng_banget('13 FBBrute')
+inu_ganteng_banget('14 Ngrok')
+inu_ganteng_banget('15 Torshammer')
+inu_ganteng_banget('16 RouterSploit')
+inu_ganteng_banget('17 Hydra')
+inu_ganteng_banget('18 Weevely')
+inu_ganteng_banget('19 SQLMap')
+inu_ganteng_banget('20 Dirbuster')
+inu_ganteng_banget('21 Pybelt')
+inu_ganteng_banget('22 Exit')
